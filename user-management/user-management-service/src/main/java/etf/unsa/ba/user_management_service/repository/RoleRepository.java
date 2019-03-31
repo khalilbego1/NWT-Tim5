70e6f5f7a86a42d44e103f91ce1b5e7158648479
lsolak <lsolak1@etf.unsa.ba>
2019-03-31 01:14:18 +0100

    user management controller

--- user-management/user-management-service/src/main/java/etf/unsa/ba/user_management_service/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package etf.unsa.ba.user_management_service.repository;
-
-import etf.unsa.ba.user_management_service.model.entity.RoleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
-}
