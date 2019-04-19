dc2d709a1452ee0ec7481f009bb58be8af9c13a5
khalilbego1 <khalilbego1@etf.unsa.ba>
2019-04-19 12:11:05 +0200

    Support for locationTransport async rabbit improvement

--- arrangements/src/main/java/nwt/microservice/arrangements/configuration/RabbitMQConfiguration.java
+++ arrangements/src/main/java/nwt/microservice/arrangements/configuration/RabbitMQConfiguration.java
@@ -13,38 +13,73 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfiguration {
     private static final String LISTENER_METHOD = "receiveMessage";
     private final String userQueueName = "user-queue";
-    @Value("${fanout.exchange}")
-    private String fanoutExchange;
+    private final String locationQueueName = "location-queue";
+    @Value("${fanout.exchange1}")
+    private String fanoutExchange1;
+    @Value("${fanout.exchange2}")
+    private String fanoutExchange2;
 
     @Bean
-    Queue queue() {
+    Queue queue1() {
         return new Queue(userQueueName, true);
     }
 
     @Bean
-    FanoutExchange exchange() {
-        return new FanoutExchange(fanoutExchange);
+    FanoutExchange exchange1() {
+        return new FanoutExchange(fanoutExchange1);
     }
 
     @Bean
-    Binding binding(Queue queue, FanoutExchange exchange) {
+    Binding binding1(Queue queue1, FanoutExchange exchange1) {
         return BindingBuilder
-                .bind(queue)
-                .to(exchange);
+                .bind(queue1)
+                .to(exchange1);
     }
 
     @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-                                             MessageListenerAdapter listenerAdapter) {
+    SimpleMessageListenerContainer container1(ConnectionFactory connectionFactory,
+                                              MessageListenerAdapter listenerAdapter1) {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(userQueueName);
-        container.setMessageListener(listenerAdapter);
+        container.setMessageListener(listenerAdapter1);
         return container;
     }
 
     @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+    MessageListenerAdapter listenerAdapter1(Receiver receiver) {
         return new MessageListenerAdapter(receiver, LISTENER_METHOD);
     }
+    @Bean
+    Queue queue2() {
+        return new Queue(userQueueName, true);
+    }
+
+    @Bean
+    FanoutExchange exchange2() {
+        return new FanoutExchange(fanoutExchange1);
+    }
+
+    @Bean
+    Binding binding2(Queue queue2, FanoutExchange exchange2) {
+        return BindingBuilder
+                .bind(queue2)
+                .to(exchange2);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container2(ConnectionFactory connectionFactory,
+                                              MessageListenerAdapter listenerAdapter2) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(userQueueName);
+        container.setMessageListener(listenerAdapter2);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter2(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, LISTENER_METHOD);
+    }
+
 }
