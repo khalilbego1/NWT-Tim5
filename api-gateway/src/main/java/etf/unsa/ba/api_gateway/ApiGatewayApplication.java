41c22e2348a2ccc4df6c1a065228d1e1177bc78d
lsolak <lsolak1@etf.unsa.ba>
2019-06-03 00:31:49 +0200

    revert

--- api-gateway/src/main/java/etf/unsa/ba/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/etf/unsa/ba/api_gateway/ApiGatewayApplication.java
@@ -2,11 +2,9 @@ package etf.unsa.ba.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableOAuth2Sso
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
